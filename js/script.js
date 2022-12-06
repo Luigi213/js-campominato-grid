3738f300dfe9d219e5a8258bd5580fdc431cab33
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-06 14:16:41 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//1- Creare bottone in html che al click genererà una griglia di gioco
+//2- Creare la griglia di gioco in js
+//2.1 ogni ha un numero progressivo, da 1 a 100, quindi 10 caselle per ognuna delle 10 righe
+//2.2 al click di una delle caselle si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+let button = document.getElementById('btn');
+button.addEventListener('click', function(){
+    function createSquare(num){
+        let elementSquare = document.createElement('div');
+
+        elementSquare.classList.add('square');
+
+        elementSquare.innerText = num;
+
+        return elementSquare;
+    }
+    
+    for(let i=0; i<100; i++){
+        const newElement = createSquare(i +1);
+        
+        grid.appendChild(newElement);
+        
+        newElement.addEventListener('click', function(){
+            this.classList.add('click')
+            console.log(i + 1)
+        });
+    }
+
+    let grid = document.getElementById('grid');
+});
